41fb82c416c5bcdb33e684420efbbc8d4db0d4df
Tomáš Hřebíček <88036219+tomas-hrebicek@users.noreply.github.com>
2023-08-28 08:44:11 +0200

    creating project and class for testing with test in program.cs

---
+++ WhyNotUseComparisonOperators/Program.cs
@@ -0,0 +1,17 @@
+﻿using WhyUseIsNull;
+
+TestClass? nullObject = null;
+
+Console.WriteLine($"nullObject == null -> {nullObject == null}");
+Console.WriteLine($"nullObject is null -> {nullObject is null}");
+
+TestClass A = new TestClass("TEST");
+TestClass B = new TestClass("TEST");
+
+Console.WriteLine($"nullObject == A -> {nullObject == B}");
+Console.WriteLine($"nullObject == B -> {nullObject == B}");
+
+Console.WriteLine($"A == B -> {A == B}");
+Console.WriteLine($"A.Equals(B) -> {A.Equals(B)}");
+
+Console.Read();
---
+++ WhyNotUseComparisonOperators/TestClass.cs
@@ -0,0 +1,43 @@
+﻿namespace WhyUseIsNull
+{
+    internal class TestClass
+    {
+        internal TestClass(string content) 
+        {
+            if (string.IsNullOrEmpty(content))
+            {
+                throw new ArgumentNullException(nameof(content));
+            }
+
+            this.Content = content;
+        }
+
+        public static bool operator == (TestClass? a, TestClass? b)
+        {
+            if (a is null)
+                return b is not null;
+            else
+                return !a.Equals(b);
+        }
+
+        public static bool operator !=(TestClass? a, TestClass? b)
+        {
+            if (a is null)
+                return b is null;
+            else
+                return a.Equals(b);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public string Content { get; init; }
+    }
+}
